8c2aed45a69d1d3dbcfb0c2e30e773ef4008ed6f
Mohammad Hassan shaikh <52702311+thehassanshaikh@users.noreply.github.com>
2023-06-23 20:21:19 +0530

    added initials file

---
+++ script.js
@@ -0,0 +1,36 @@
+//function for clock
+
+window.onload = function() {
+    clock();  
+      function clock() {
+      var now = new Date();
+      var TwentyFourHour = now.getHours();
+      var hour = now.getHours();
+      var min = now.getMinutes();
+      var sec = now.getSeconds();
+      var mid = 'pm';
+      if(sec < 10) { 
+        sec = "0" + sec; 
+      }
+      if (min < 10) {
+        min = "0" + min;
+      }
+      if (hour > 12) {
+        hour = hour - 12;
+      }   
+      if (hour < 10 ) {
+        hour = "0" + hour;
+      }   
+      if(hour==0){ 
+        hour=12;
+      }
+      if(TwentyFourHour < 12) {
+         mid = 'am';
+      }
+    document.getElementById('currentTime').innerHTML =     hour+':'+min+':'+sec +' '+mid ;
+      setTimeout(clock, 1000);
+      }
+  }
+  
+
+// clock function end
