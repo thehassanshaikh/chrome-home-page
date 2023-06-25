5a5c3e231c3abfcef11f7d8ec3af121d974ee748
Mohammad Hassan shaikh <thehasanshaikh@gmail.com>
2023-06-25 21:24:41 +0530

    added date and week day

--- script.js
+++ script.js
@@ -1,36 +1,53 @@
 //function for clock
 
-window.onload = function() {
-    clock();  
-      function clock() {
-      var now = new Date();
-      var TwentyFourHour = now.getHours();
-      var hour = now.getHours();
-      var min = now.getMinutes();
-      var sec = now.getSeconds();
-      var mid = 'pm';
-      if(sec < 10) { 
-        sec = "0" + sec; 
-      }
-      if (min < 10) {
-        min = "0" + min;
-      }
-      if (hour > 12) {
-        hour = hour - 12;
-      }   
-      if (hour < 10 ) {
-        hour = "0" + hour;
-      }   
-      if(hour==0){ 
-        hour=12;
-      }
-      if(TwentyFourHour < 12) {
-         mid = 'am';
-      }
-    document.getElementById('currentTime').innerHTML =     hour+':'+min+':'+sec +' '+mid ;
-      setTimeout(clock, 1000);
-      }
+window.onload = function () {
+  clock();
+  function clock() {
+    var now = new Date();
+    var TwentyFourHour = now.getHours();
+    var hour = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+    var mid = 'pm';
+    if (sec < 10) {
+      sec = "0" + sec;
+    }
+    if (min < 10) {
+      min = "0" + min;
+    }
+    if (hour > 12) {
+      hour = hour - 12;
+    }
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (hour == 0) {
+      hour = 12;
+    }
+    if (TwentyFourHour < 12) {
+      mid = 'am';
+    }
+    document.getElementById('currentTime').innerHTML = hour + ':' + min + ':' + sec + ' ' + mid;
+    setTimeout(clock, 1000);
+
+    // to display date
+    const todaysDate = now.toLocaleDateString();
+
+    document.getElementById('todayDate').innerHTML = todaysDate;
+
+    //to dispay Day
+    let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satarday"];
+    let dayy = now.getDay();
+    document.getElementById('todayDay').innerHTML = weekdays[dayy]
+
+
   }
-  
+
+
+
+}
+
 
 // clock function end
+
+
